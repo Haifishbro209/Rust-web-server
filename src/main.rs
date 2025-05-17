53b2904dbac0a94547f10160db93227596bd8b43
haifishbro209 <kaloyannikolov016@gmail.com>
2025-05-17 17:43:11 +0200

    something is wrong

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,34 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
+fn problem_notification(problem_message:String){
+    loop {
+        println!("very bad!!! {}",problem_message);
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
+    let listener =match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listerer) => listerer,
+        Err(error) => 
+        {
+            println!("Fehler beim serverstart {}", error);
+            return;
+        }
+    };
+
+    loop{
+        match listener.accept() {
+            Ok((stream, adr)) => {
+                print!("stream {}",adr);
+                
+            }
+            Err(error) => {
+                let stringed_error = format!("{}",error);
+                problem_notification(stringed_error);
+            }
+        }
+    }
 }
